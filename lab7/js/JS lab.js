babcc718444b9e3964b5f6ef247a0bfffff44444
LianaPS <lsimonel@ucsc.edu>
2024-10-27 15:45:13 -0700

    changes

---
+++ lab7/js/JS lab.js
@@ -0,0 +1,26 @@
+// index.js - Lab 7 Functions
+// Author: Liana Simonelli
+// Date: 27 October 2024
+
+// Constants
+
+// Functions
+
+$(document).ready(function() {
+    $('#script-output').text("This is the output from the JavaScript file!");
+});
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
